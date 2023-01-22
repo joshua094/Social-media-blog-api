6d3931e29440ad7579e0529060b8c44ce36f2370
Emmanuel Amoo <amooemma10@gmail.com>
2023-01-22 07:36:04 +0100

    security

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes/user-routes';
 import blogRouter from './routes/blog-routes';
+import dotenv from 'dotenv'
 
 
+dotenv.config()
 const app = express();
 app.use(express.json());
+
 
 app.use("/api/user",router)
 app.use("/api/blog", blogRouter)
 
 mongoose.set('strictQuery', false)
-mongoose.connect('mongodb+srv://admin:E7S8EhZCMlHB83BT@cluster0.vknoc1r.mongodb.net/Blog?retryWrites=true&w=majority'
-)
+mongoose.connect(process.env.MONGO_DB_URL)
 .then( () => {
     app.listen(5000) 
 })
